49edf69b2759aee99b711b551d60bc825fce3b5c
Trevor Terris <tjterris@gmail.com>
2015-05-12 22:02:53 -0400

    coffee and human

---
+++ coffee.rb
@@ -0,0 +1,37 @@
+class Coffee
+
+def initialize(coffee)
+
+  @coffee = coffee
+
+
+def full?
+true if tsmf = true || false if tsmf = false
+end
+
+
+def empty?
+  true
+end
+
+
+def tsmf
+    false
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
+end
---
+++ human.rb
@@ -0,0 +1,40 @@
+class Human
+
+  def initialize(name)
+    @name = name
+    @alertness = 0.0
+
+    #@alertness = 0.0
+
+  end
+
+  def alertness
+    @alertness
+  end
+
+  def has_coffee?
+  
+  false
+
+  end
+
+  def needs_coffee?
+  
+  true
+  
+  end
+
+
+  def buy(tsmf)
+
+   @tsmf=tsmf
+  end
+
+  def drink!
+    true
+  end
+
+
+
+
+end
